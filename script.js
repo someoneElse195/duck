39fde689d92ec8106fc1b894459b538280d56f5d
Ash <amwest195@gmail.com>
2021-07-26 16:15:44 -0500

    sus3

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const sound = "https://f000.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z9b2a7e929faa13a473850510_f10306c64124d325b_d20210310_m062616_c000_v0001079_t0021";
+
 let player;
 let status = 0;
 let buffer = ["", "", ""];
-let styleSheet = document.createElement("style");
 
 let style1 = `body {
-    background-image: url(https://i1.sndcdn.com/artworks-Uii8SMJvNPxy8ePA-romBoQ-t500x500.jpg);
-}`;
+                background-image: url(https://i1.sndcdn.com/artworks-Uii8SMJvNPxy8ePA-romBoQ-t500x500.jpg);
+              }`;
 
+let styleSheet = document.createElement("style");
+
+/**
+ * listen for keystroke patterns
+ *
+ * @param event key event
+ */
 function keyEvent(event) {
     buffer.shift();
     buffer.push(event.key);
@@ -30,5 +37,20 @@ function keyEvent(event) {
             buffer = ["", "", ""];
             status = 0;
         }
-    }
+    } 
+}
+
+/**
+ * initialize the listener
+ */
+function scriptInit() {
+    player = document.querySelector("#player");
+    document.addEventListener("keypress", keyEvent);
+}
+
+/**
+ * load script on load, as well as resize script
+ */
+window.onload = () => {
+    scriptInit();
 }
